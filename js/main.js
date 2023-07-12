beaa3ae6187eb064ac96b49fdb40e8cb6d877bc0
VitaliySotskov <vit6150@gmail.com>
2023-07-13 00:33:43 +0300

    8.14. Открывается и закрывается

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import {getPictures} from './data.js';
 import {renderPictures} from './thumbnails.js';
+import {renderGallery} from './gallery.js';
 
 const arrPictures = getPictures();
 renderPictures(arrPictures);
+
+renderGallery(arrPictures);
