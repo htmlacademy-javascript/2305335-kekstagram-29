963fe4ae076448b7997fe929e45c183fb8d3da0c
VitaliySotskov <vit6150@gmail.com>
2023-07-21 15:47:15 +0300

    9.15. Помощь друга

---
+++ js/filters.js
@@ -0,0 +1,132 @@
+const EFFECTS = [
+  {
+    name : 'original',
+    style : 'none',
+    min : 0 ,
+    max : 100,
+    step : 1,
+    unit : '',
+  },
+  {
+    name : 'chrome',
+    style : 'grayscale',
+    min : 0 ,
+    max : 1,
+    step : 0.1,
+    unit : '',
+  },
+  {
+    name : 'sepia',
+    style : 'sepia',
+    min : 0,
+    max : 1,
+    step : 0.1,
+    unit : '',
+  },
+  {
+    name : 'marvin',
+    style : 'invert',
+    min : 0,
+    max : 100,
+    step : 1,
+    unit : '%',
+  },
+  {
+    name : 'phobos',
+    style : 'blur',
+    min : 0,
+    max : 3,
+    step : 0.1,
+    unit : 'px',
+  },
+  {
+    name : 'heat',
+    style : 'brightness',
+    min : 1,
+    max : 3,
+    step : 0.1,
+    unit : '',
+  }
+];
+const DEFAULT_EFFECT = EFFECTS[0];
+let currentEffect = DEFAULT_EFFECT;
+
+const sliderContainer = document.querySelector('.img-upload__effect-level');
+const sliderElement = document.querySelector('.effect-level__slider');
+const effectSliderValue = document.querySelector('.effect-level__value');
+const effectsList = document.querySelector('.img-upload__effects');
+const imagePreview = document.querySelector('.img-upload__preview img');
+
+const showSliderContainer = () => sliderContainer.classList.remove('hidden');
+const hideSliderContainer = () => sliderContainer.classList.add('hidden');
+
+
+const createSlider = () => {
+  noUiSlider.create(sliderElement, {
+    start: DEFAULT_EFFECT.max,
+    range: {
+      min: DEFAULT_EFFECT.min,
+      max: DEFAULT_EFFECT.max,
+    },
+    step: DEFAULT_EFFECT.step,
+    connect: 'lower',
+  });
+};
+
+const isDefault = () => currentEffect === DEFAULT_EFFECT;
+
+const updateSettingsSlider = () => {
+  sliderElement.noUiSlider.updateOptions({
+    start: currentEffect.max,
+    range: {
+      min: currentEffect.min,
+      max: currentEffect.max,
+    },
+    step: currentEffect.step,
+    connect: 'lower',
+  });
+
+};
+
+const onEffectsChange = (evt) => {
+  currentEffect = EFFECTS.find((element) => element.name === evt.target.value);
+  if (!isDefault()) {
+    updateSettingsSlider();
+    showSliderContainer();
+  } else {
+    updateSettingsSlider();
+    hideSliderContainer();
+  }
+
+};
+
+const onSliderUpdate = () => {
+  const sliderValue = sliderElement.noUiSlider.get();
+  if (isDefault()) {
+    imagePreview.style.filter = DEFAULT_EFFECT.style;
+  } else{
+    imagePreview.style.filter = `${currentEffect.style}(${sliderValue}${currentEffect.unit})`;
+  }
+  effectSliderValue.value = sliderValue;
+};
+
+const addEffectListener = () => {
+  effectsList.addEventListener('change', onEffectsChange);
+};
+
+const removeEffectListener = () => {
+  effectsList.removeEventListener('change', onEffectsChange);
+};
+
+const setDefaultSlider = () => {
+  createSlider();
+  hideSliderContainer();
+  sliderElement.noUiSlider.on('update', onSliderUpdate);
+};
+
+const resetEffects = () => {
+  currentEffect = DEFAULT_EFFECT;
+  updateSettingsSlider();
+};
+
+export {setDefaultSlider, resetEffects,addEffectListener,removeEffectListener};
--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import {getPictures} from './data.js';
 import {renderGallery} from './gallery.js';
 import {renderModalForm} from './form.js';
+import {setDefaultSlider} from './filters.js';
 
 const arrPictures = getPictures();
 
 renderGallery(arrPictures);
 
 renderModalForm();
+setDefaultSlider();
