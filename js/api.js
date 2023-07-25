48cb43d8f828af939eeceb8555198de6382bf5a4
VitaliySotskov <vit6150@gmail.com>
2023-07-26 01:04:27 +0300

    Исправление ошибок

--- js/api.js
+++ js/api.js
@@ -1,18 +1,17 @@
 const BASE_URL = 'https://29.javascript.pages.academy/kekstagram';
+
 const Route = {
   GET_DATA: '/data',
   SEND_DATA: '/',
 };
-const Method = {
-  GET: 'GET',
-  POST: 'POST',
-};
+
 const ErrorText = {
   GET_DATA: 'Не удалось загрузить данные. Попробуйте обновить страницу',
   SEND_DATA: 'Не удалось отправить форму. Попробуйте ещё раз',
 };
-const load = (route, errorText, method = Method.GET, body = null) =>
-  fetch(`${BASE_URL}${route}`, {method, body})
+
+const getData = () =>
+  fetch(`${BASE_URL}${Route.GET_DATA}`)
     .then((response) => {
       if (!response.ok) {
         throw new Error();
@@ -20,11 +19,24 @@ const load = (route, errorText, method = Method.GET, body = null) =>
       return response.json();
     })
     .catch(() => {
-      throw new Error(errorText);
+      throw new Error(ErrorText.GET_DATA);
     });
 
-const getData = () => load(Route.GET_DATA, ErrorText.GET_DATA);
-
-const sendData = (body) => load(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
+const sendData = (body, onSuccess) =>
+  fetch(`${BASE_URL}${Route.SEND_DATA}`,
+    {
+      method: 'POST',
+      body,
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error();
+      } else {
+        onSuccess();
+      }
+    })
+    .catch(() => {
+      throw new Error(ErrorText.SEND_DATA);
+    });
 
 export {getData, sendData};
--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
-//import {getPictures} from './data.js';
 import {renderGallery} from './gallery.js';
 import {renderModalForm} from './form.js';
 import {setDefaultSlider} from './filters.js';
 import {getData} from './api.js';
 import {setUserFormSubmit} from './form.js';
 import {showAlert} from './utils.js';
-
-// const arrPictures = getPictures();
-
-// renderGallery(arrPictures);
 
 getData().then((picturesArray)=>{
   renderGallery(picturesArray);
